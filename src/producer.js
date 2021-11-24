0a0e4fd98a62d6f17ae2664589bba25244f189ea
Patryk Wegrzyn <p.wegrzyn@cloudfmgroup.com>
2021-11-24 16:07:21 +0000

    fix(producer) do not send empty messages

--- src/producer.js
+++ src/producer.js
@@ -1,4 +1,4 @@
-const { Writable } = require("stream");
+const { Writable } = require('stream');
 
 class Producer extends Writable {
   constructor(topic, producer) {
@@ -10,12 +10,15 @@ class Producer extends Writable {
   _write(batch, enc, callback) {
     const topic = this.topic;
     const messages = batch.length ? batch : batch.messages;
+    if (messages.length === 0) {
+      return callback();
+    }
     this.producer
       .send({ topic, messages })
       .then((something) => {
         if (!batch.length) {
           const { highWatermark, partition } = batch;
-          this.emit("produced", { partition, offset: highWatermark });
+          this.emit('produced', { partition, offset: highWatermark });
         }
         callback();
       })
